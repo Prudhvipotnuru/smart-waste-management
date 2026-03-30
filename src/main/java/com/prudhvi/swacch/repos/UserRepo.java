aa5a27f77166e0ef15b3b2631c88098854e5de07
prudhvipotnuru <prudhvipotnuru@gmail.com>
2026-03-30 22:42:54 +0530

    Spring Security with JWT

--- src/main/java/com/prudhvi/swacch/repos/UserRepo.java
+++ src/main/java/com/prudhvi/swacch/repos/UserRepo.java
@@ -1,11 +1,13 @@
 package com.prudhvi.swacch.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.prudhvi.swacch.model.User;
 
 @RepositoryRestResource(path = "user")
-public interface UserRepo extends JpaRepository<User, Long>{
-
+public interface UserRepo extends JpaRepository<User, Long> {
+	Optional<User> findByName(String name);
 }
