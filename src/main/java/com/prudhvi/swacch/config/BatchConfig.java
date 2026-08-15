ea09b3505a814b1ad5212c57ddde377239702f6c
prudhvipotnuru <prudhvipotnuru@gmail.com>
2026-08-15 08:33:53 +0530

    chunk size is increased to 500

--- src/main/java/com/prudhvi/swacch/config/BatchConfig.java
+++ src/main/java/com/prudhvi/swacch/config/BatchConfig.java
@@ -125,7 +125,7 @@ public class BatchConfig {
 	public Step housesStep(JobRepository jobRepository,FlatFileItemReader<House> reader,HouseProcessor processor,
 			ClassifierCompositeItemWriter<House> writer,FlatFileItemWriter<House> errorWriter) throws IOException {
 		return new StepBuilder(jobRepository)
-			    .<House, House>chunk(100)
+			    .<House, House>chunk(500)
 			    .reader(reader)
 			    .processor(processor)
 			    .writer(writer)
